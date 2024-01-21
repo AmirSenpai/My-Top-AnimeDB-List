dac03682b968412cf92c102862fa2d585dfa071c
Amir <amirhoosindoosti15@gmail.com>
2024-01-22 01:46:35 +0330

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/My-Top-AnimeDB-List/',
   plugins: [react()],
 });
