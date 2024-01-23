16e305f1e27e32b1297b75bf05e5b54e16802017
Amir <amirhoosindoosti15@gmail.com>
2024-01-23 18:52:26 +0330

    added a new anime

--- src/Anime/AnimeInfos.js
+++ src/Anime/AnimeInfos.js
@@ -22,24 +22,24 @@ export default [
       "https://media.publit.io/file/fullmetal-alchemist-brotherhood-blu-ray-kv-1154021.jpg",
     ARating: "10/10",
     MRating: "9/10",
-    Episode: "64Ep | 1Se",
+    Episode: "64 Eps | 1 Seasons",
     Description:
       "After a horrific alchemy experiment goes wrong in the Elric household, brothers Edward and Alphonse are left in a catastrophic new reality. Ignoring the alchemical principle banning human transmutation, the boys attempted to bring their recently deceased mother back to life...",
     Rank: "#2",
   },
 
-  // {
-  //   EngName: "lorem",
-  //   JpName: "lorem",
-  //   Date: "2023 - 2024",
-  //   Genre: "Shonen",
-  //   Picture: "src/",
-  //   ARating: "10/10",
-  //   MRating: "9/10",
-  //   Episode: "12Eps",
-  //   Description: "lorem lorem lorem",
-  //   Rank: "#3",
-  // },
+  {
+    EngName: "FireForce",
+    JpName: "炎炎ノ消防隊",
+    Date: "2019 - 2020",
+    Genre: " Action, Fantasy",
+    Picture: "https://media.publit.io/file/bvdZcGpE6YLI61Yuv40j45pxGut.png",
+    ARating: "9/10",
+    MRating: "7/10",
+    Episode: "48 Eps | 2 Seasons",
+    Description: "Spontaneous Human Combustion: a chaotic phenomenon that has plagued humanity for years, randomly transforming ordinary people into flaming, violent creatures known as Infernals,Young and eager third-generation pyrokinetic Shinra Kusakabe, nicknamed Devil's Footprints for his explosive ability to ignite his feet at will...",
+    Rank: "#3",
+  },
 
   // {
   //   EngName: "lorem",
